fb86e4a6cfb30c3955d62e94743f4e3bf38334de
Aknmoon <akinomi0603@gmail.com>
2023-08-21 22:42:07 +0900

    javascript復習

--- index.js
+++ index.js
@@ -4,22 +4,52 @@ let unko = "Hello World";
 
 // unko = "Hello World2";
 
+// console.log(unko);
+
 // 定数
 const bigEn = "ENHYPEN";
 
 // bigEn = "Hello World2!"
+// これはできない↑
+
+// console.log(bigEn);
+
+// 配列
+let king = ["one","two","three","four"];
+
+// console.log(king);
+// console.log(king[2]);
 
 const andteam = ["one","two","three","four"];
 
 // console.log(andteam[0]);
 
 // loop
+
+// let index = 0;
+// while(index < 5){
+
+//   console.log(index);
+//   index++;
+// }
+
+
+
+// let index = 0;
+// while(index < andteam.length){
+
+//   console.log(index);
+//   index++;
+//    ++はプラス1
+// }
+
+
 // let index = 0;
 // while(index < andteam.length){
 
 //   console.log(andteam[index]);
 //   index++;
-  // ++はプラス1
+//    ++はプラス1
 // }
 
 
@@ -30,7 +60,19 @@ const andteam = ["one","two","three","four"];
 //   console.log("Bite Me");
 // }
 
+
+
+
 // 関数
+// const test = () =>{
+//   if(andteam.length > 5){
+//     console.log("firework");
+//   }else{
+//     console.log("Bite Me");
+//   }
+// };
+// test();
+
 const test = (arg) =>{
   if(andteam.length > arg){
     console.log("firework");
@@ -39,7 +81,13 @@ const test = (arg) =>{
   }
 };
 
+//  test(3);
+//  testの3がargに代入される   argは引数という意味
+// test(9);
 // test(6);
+// test(1);
+
+
 
 
 // object
@@ -51,13 +99,21 @@ const blossom = {
     console.log("Hello");
   }
 };
-
 // console.log(blossom);
+// console.log(blossom.color);
+// console.log(blossom.flower());
 
+
+
+
+// console.log(window);
 // console.log(window.innerWidth);
+
+
 // window.alert("Hello World");
 
 // console.log(document);
+// console.log(document.getElementsByTagName ('button'));
 // console.log(document.getElementsByTagName ('button')[0]);
 document.getElementsByTagName ('button')[0].addEventListener("click", ()=> {
   window.alert("Hello World");
